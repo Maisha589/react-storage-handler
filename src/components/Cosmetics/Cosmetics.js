2360a92b09cb74056f2ed238742a26162f96581e
Maisha589 <maisharahman589@gmail.com>
2022-04-05 17:27:25 +0600

    first commit

---
+++ src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { getTotal } from '../../utilities/calculate';
+import Cosmetic from '../cosmetic/Cosmetic';
+
+const Cosmetics = () => {
+    const [cosmetics, setCosmetics] = useState([]);
+
+useEffect(() => {
+    fetch("data.json")
+    .then(res => res.json())
+    .then(data => setCosmetics(data))
+}, [])
+
+const total = getTotal(cosmetics);
+
+    return (
+        <div>
+            <h1>Welcome to my cosmetics store</h1>
+            <p>Money needed: {total}</p>
+            {
+                cosmetics.map(cosmetic => <Cosmetic 
+                    key={cosmetic.id}
+                    cosmetic ={cosmetic}
+                    ></Cosmetic>)
+            }
+       </div>
+    );
+};
+
+export default Cosmetics;
